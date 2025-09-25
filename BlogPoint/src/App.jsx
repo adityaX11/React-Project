532c57d9c0ed7981148f16196500dce882fcd77d
Aditya Kumar <adityakumarsah4555@gmail.com>
2025-09-26 05:06:37 +0530

    just add the main env

--- BlogPoint/src/App.jsx
+++ BlogPoint/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 function App() {
-
+  console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <>
       <h1>श्री गणेशाय नमः, जय सरस्वती माता, जय श्री राम, हर हर महादेव</h1>
