e982e1402bfa7b3e4678b912130c9ee2278f585a
Aditya Kumar <adityakumarsah4555@gmail.com>
2025-09-27 04:28:17 +0530

    learn the auth services features of the projects.

---
+++ BlogPoint/src/appwrite/auth.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-useless-catch */
+
+// this whole code is for auth for users.
+import conf from "../conf/conf";
+import {Client,Account,ID} from "appwrite"
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account=new Account(this.client);
+    }
+
+    async createAccount({email,password,name}){
+        try {
+            const userAccount=await this.account.create(ID.unique(),email,password,name);
+            if(userAccount){
+                // call another method like redirect the login page.
+                return this.login({email,password});
+            }
+            else{
+                return userAccount;
+            }
+        } catch (error) { // here may be problem
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error",error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error",error);
+        }
+    }
+}
+
+const authsService=new AuthService();
+export default authsService;
